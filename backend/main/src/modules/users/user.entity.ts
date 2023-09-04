13fd79218203a2676fe06dc93289fbc7f898c882
carlos.gutfer <carlos.gutfer@hotmail.es>
2023-09-04 12:36:58 +0200

    [MOD] Add new columns and coments entity user

--- backend/main/src/modules/users/user.entity.ts
+++ backend/main/src/modules/users/user.entity.ts
@@ -3,6 +3,21 @@ import { Table, Column, Model, DataType } from 'sequelize-typescript';
 @Table
 export class User extends Model<User> {
 
+    /** 
+    * Class used to create the table User
+    *
+    * @param name - Name of the user
+    * @param firstname - first name of the user
+    * @param secondname - if have, second name of the user
+    * @param permiss - type of user
+    * @param email - email of the user
+    * @param gender - gender of the user
+    * @param adress - Place where the user live
+    * @param password - password to login 
+    * @param phone1 - number of contact
+    * @param phone2 - second number of contact
+    */
+
     @Column({
         type: DataType.STRING,
         allowNull: false,
